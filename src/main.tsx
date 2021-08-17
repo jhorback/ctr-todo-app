fed8bd1c1900b3b7c9099316bce27a6ff54bf239
John Horback <jhorback@gmail.com>
2021-08-17 16:09:56 -0400

    Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux';
 import {store} from "./app/store";
-import './index.scss'
+import './AppDocumentStyles.scss'
 import App from './App'
 
 ReactDOM.render(
